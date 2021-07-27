ec1b133fdb56549fd67595b43a1fee9e90e1e8a3
tarasduda <tarasduda8@gmail.com>
2021-07-27 18:22:20 +0300

    finish home work js

---
+++ task-1.js
@@ -0,0 +1,18 @@
+
+// let userName = 'Vova'
+// let status = 'Moderator'
+
+// // Concatenation  склейка строк
+// let userInfo = userName + ' ' + status + ' ' + 'hello'
+// console.log(userInfo)
+
+
+
+
+
+// let name = 'Генератор защитного поля'
+// let price = '1000'
+
+// console.log(`Выбран "${name}", цена за штуку ${price} кредитов`)
+// price = '2000'
+// console.log(`Выбран "${name}", цена за штуку ${price} кредитов`)
